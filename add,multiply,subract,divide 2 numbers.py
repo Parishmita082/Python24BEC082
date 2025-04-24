bf4eb5e4bac62c4a1baeebc71e72a279f66f3204
Parishmita082 <24bec082@sot.pdpu.ac.in>
2025-04-24 21:32:03 +0530

    Delete add,multiply,subract,divide 2 numbers.py

--- add,multiply,subract,divide 2 numbers.py
+++
@@ -1,7 +0,0 @@
-#add,multiply,subract,divide 2 numbers
-a=int(input("enter number a: "))
-b=int(input("enter number b(a>b,b!=0): "))
-print("sum=",a+b)
-print("product=",a*b)
-print("difference=",a-b)
-print("quotient=",a/b)
